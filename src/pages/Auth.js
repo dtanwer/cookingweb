6c9cbeb742e569d7ff1c25d6850b67f7f443c27b
Deepak <depak@gmail.com>
2023-04-15 22:23:29 +0530

    chech in Auth

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -52,7 +52,7 @@ const Register=()=>{
   const onSubmit= async (event)=>{
     event.preventDefault();
     try{
-      await axios.post("http://localhost:3001/auth/register",{
+      await axios.post("https://cooking-api.vercel.app/auth/register",{
         username,
         password,
       });
